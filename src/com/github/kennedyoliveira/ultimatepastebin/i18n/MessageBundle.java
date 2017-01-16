b7a012d75052ea890487da9d67dd7a0ac4f5e0ea
Kennedy Oliveira <kennedy.oliveira@outlook.com>
2017-01-15 23:53:43 -0200

    1.5.0 release preparation - Fix file charset

--- src/com/github/kennedyoliveira/ultimatepastebin/i18n/MessageBundle.java
+++ src/com/github/kennedyoliveira/ultimatepastebin/i18n/MessageBundle.java
@@ -23,7 +23,7 @@ public class MessageBundle {
     availableTranslations = new HashMap<>();
 
     availableTranslations.put("German", new Locale.Builder().setLanguage("de").build());
-    availableTranslations.put("PortuguÃªs Brasileiro", new Locale.Builder().setLanguage("pt").setRegion("BR").build());
+    availableTranslations.put("Português Brasileiro", new Locale.Builder().setLanguage("pt").setRegion("BR").build());
     availableTranslations.put("English", new Locale.Builder().setLanguage("en").build());
     availableTranslations.put("French", new Locale.Builder().setLanguage("fr").build());
     availableTranslations.put("Norwegian", new Locale.Builder().setLanguage("no").build());
