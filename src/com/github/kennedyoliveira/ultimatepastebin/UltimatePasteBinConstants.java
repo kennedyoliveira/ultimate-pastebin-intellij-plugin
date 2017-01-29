8698fb9016b56123ab256b7d9b07d2e35a048f2f
Kennedy Oliveira <kennedy.oliveira@outlook.com>
2017-01-28 22:10:12 -0200

    1.5.1 release - Fix #14

--- src/com/github/kennedyoliveira/ultimatepastebin/UltimatePasteBinConstants.java
+++ src/com/github/kennedyoliveira/ultimatepastebin/UltimatePasteBinConstants.java
@@ -8,7 +8,7 @@ public final class UltimatePasteBinConstants {
   /**
    * Plugin Version
    */
-  public static final String VERSION = "1.5.0";
+  public static final String VERSION = "1.5.1";
   /**
    * URL For donating
    */
